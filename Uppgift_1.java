54a0f382419e18a30e12c913c03b0ded72cc42eb
GabrielBladh <gabrielbladh@hotmail.com>
2025-09-29 20:25:13 +0200

    tog bort =

--- Uppgift_1.java
+++ Uppgift_1.java
@@ -4,11 +4,11 @@ public class Uppgift_1
     {
         int[] nummer = {13, 42, 56, 78, 19, 45, 57, 34, 77, 2};
         int SöktVärde = 57;
-        for (int i = 0; i <= nummer.length; i++ )
+        for (int i = 0; i < nummer.length; i++ )
         {
             if (nummer[i] == SöktVärde)
             {
-                System.out.printf("Nummer" + nummer[i] + "finns på raden " +i );
+                System.out.printf("Nummer " + nummer[i] + " finns på raden " +i );
             }
         }
     }
